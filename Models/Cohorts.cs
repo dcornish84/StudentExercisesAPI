aa9291603a1cf85aabe2db287df79f1e099d27f7
David <dcornish84@gmail.com>
2019-12-16 14:36:32 -0600

    studentcontroller

--- Models/Cohorts.cs
+++ Models/Cohorts.cs
@@ -7,5 +7,10 @@ namespace StudentExercisesAPI.Models
 {
     public class Cohorts
     {
+        public int Id { get; set; }
+
+        public string CohortName { get; set; }
+        public List<Students> students { get; set; } = new List<Students>();
+        public List<Instructors> instructors { get; set; } = new List<Instructors>();
     }
 }
