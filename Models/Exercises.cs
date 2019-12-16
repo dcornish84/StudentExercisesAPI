20fd34d98c51a5df173a011e763aab36af2de80a
David <dcornish84@gmail.com>
2019-12-15 23:53:04 -0600

    first commit

---
+++ Models/Exercises.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentExercisesAPI.Models
+{
+    
+   public class Exercises
+    {
+
+        public int Id { get; set; }
+
+        public string ExerciseName { get; set; }
+
+        public string ProgrammingLanguage { get; set; }
+
+
+    }
+}
