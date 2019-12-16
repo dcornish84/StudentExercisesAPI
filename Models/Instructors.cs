180191101512f4876a88ca24eb3e02e66cbb7ddd
David <dcornish84@gmail.com>
2019-12-15 23:57:55 -0600

    updated

--- Models/Instructors.cs
+++ Models/Instructors.cs
@@ -7,5 +7,22 @@ namespace StudentExercisesAPI.Models
 {
     public class Instructors
     {
+
+     
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string SlackHandle { get; set; }
+
+        public string Specialty { get; set; }
+
+        public int CohortId { get; set; }
+
+        public string CohortName { get; set; }
+
+
     }
 }
