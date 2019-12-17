d1c60fa03b838d81354bb276719271b8e181d77d
David <dcornish84@gmail.com>
2019-12-17 16:00:42 -0600

    part 7 done

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -33,50 +33,65 @@ namespace StudentExercisesAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get([FromQuery]int Id, string include)
         {
             using (SqlConnection conn = Connection)
             {
                 conn.Open();
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = @"SELECT s.Id, s.FirstName, s.LastName, s.SlackHandle, 
-                                       s.CohortId, c.CohortName,
-                                       se.ExerciseId, e.ExerciseName, e.ProgrammingLanguage
-                                  FROM Student s INNER JOIN Cohort c ON s.CohortId = c.id
-                                       LEFT JOIN StudentExercises se on se.StudentId = s.id
-                                       LEFT JOIN Exercise e on se.ExerciseId = e.Id";
-                    SqlDataReader reader = cmd.ExecuteReader();
                     List<Students> students = new List<Students>();
-
+                    // if they are looking for exercises
+                    if (include != null)
+                    {
+                        cmd.CommandText = @"SELECT s.Id, s.FirstName, s.LastName, s.SlackHandle, s.CohortId, e.Id AS ExerciseId, e.ExerciseName, e.ProgrammingLanguage, c.CohortName, c.Id AS CoId
+                                        FROM Student s
+                                        LEFT JOIN Cohort c ON s.CohortId = c.Id
+                                        INNER JOIN  StudentExercises se ON se.StudentId = s.Id
+                                        INNER JOIN Exercise e ON e.Id = se.ExerciseId";
+                    }
+                    else
+                    {
+                        cmd.CommandText = @"SELECT s.Id, s.FirstName, s.LastName, s.SlackHandle, s.CohortId, c.Id AS CoId, c.CohortName 
+                                        FROM Student s
+                                        LEFT JOIN  Cohort c ON s.CohortId = c.Id";
+                    }
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    //List<Exercise> this is where we are storing all of the exercises from the sql statement
                     while (reader.Read())
                     {
-
-
-                            Students student = new Students
+                        List<Exercises> exercises = new List<Exercises>();
+                        if (include != null)
+                        {
+                            Exercises exercise = new Exercises
                             {
                                 Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
-                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
-                                SlackHandle = reader.GetString(reader.GetOrdinal("SlackHandle")),
-                                CohortId = reader.GetInt32(reader.GetOrdinal("CohortId")),
-                                Cohort = new Cohorts()
-                                {
-                                    CohortName = reader.GetString(reader.GetOrdinal("CohortName")),
-                                    Id = reader.GetInt32(reader.GetOrdinal("CohortId"))
-                                }
+                                ExerciseName = reader.GetString(reader.GetOrdinal("ExerciseName")),
+                                ProgrammingLanguage = reader.GetString(reader.GetOrdinal("ProgrammingLanguage"))
                             };
-
-                            students.Add(student);
-
-
-
+                            exercises.Add(exercise);
                         }
-                        reader.Close();
-
-                        return Ok(students);
+                        Students student = new Students
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            SlackHandle = reader.GetString(reader.GetOrdinal("SlackHandle")),
+                            CohortId = reader.GetInt32(reader.GetOrdinal("CohortId")),
+                            Cohort = new Cohorts()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("CoId")),
+                                CohortName = reader.GetString(reader.GetOrdinal("CohortName")),
+                            },
+                            //this is considered all of the student exercises they are currently working on
+                            Exercises = exercises
+                        };
+                        students.Add(student);
                     }
+                    reader.Close();
+                    return Ok(students);
                 }
             }
         }
     }
+}
